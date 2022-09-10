94635f908c6da7bf9aec334094589a04d2b0bce9
polyovik <taisiia.boroda@gmail.com>
2022-09-10 14:28:39 +0300

    add custom cat button

--- practice.js
+++ practice.js
@@ -1,24 +1,51 @@
-const chigirDiv = document.getElementById("chigir");
+const catsDiv = document.getElementById("chigir");
 /* chigirDiv.append("agurk") */
 
 let chigirItem = `<div class="chigir-item"> <img src="./img/chigir.jpg" alt=""> </div>`
 let agurkItem = `<div class="agurk-item"> <img src="./img/agurk.jpg" alt=""> </div>`
+let customCatItem = `<div class="custom-cat-item"> <img src="./img/custom-cat.jpg" alt=""> </div>`
 
 let addAgurkButton = document.getElementById("agurk-button");
 console.log(addAgurkButton)
+let clearChigirButton = document.getElementById("clear-chigir");
+let addChigirButton = document.getElementById("chigir-button");
+let catNameInput = document.getElementById("cat-name");
+let catNameButton = document.getElementById("show-cat-name");
+let addCustomCatButton = document.getElementById("custom-cat");
+
+function addCat(catItem, catName) {
+    const catNameText = /* '<h1>'  + catName +  '</h1>' */ `<h1> ${catName} </h1>`
+    catsDiv.innerHTML =  catsDiv.innerHTML + catItem + catNameText;
+};
 
 function addAgurk() {
-    chigirDiv.innerHTML =  chigirDiv.innerHTML + agurkItem
-    console.log("aaaaaaaa");
+    addCat(agurkItem, "Agurk")
 };
-addAgurkButton.onclick = addAgurk;
-
 /* p.onclick = function(event) { alert("moot!"); }; */
-
-let clearChigirButton = document.getElementById("clear-chigir");
 function clearChigir() {
-    chigirDiv.textContent = "";
+    catsDiv.textContent = "";
 };
+
+function addChigir() {
+    addCat(chigirItem, "Chigir") 
+};
+
+function showCatName() {
+    console.log(catNameInput.value)
+}
+
+function addCustomCat() {
+    if (catNameInput.value.length > 0) {
+        addCat(customCatItem, catNameInput.value);
+        catNameInput.value = "";
+    }
+}
+
+
+addAgurkButton.onclick = addAgurk;
+addChigirButton.onclick = addChigir;
 clearChigirButton.onclick = clearChigir;
+catNameButton.onclick = showCatName;
+addCustomCatButton.onclick = addCustomCat;
 
 
